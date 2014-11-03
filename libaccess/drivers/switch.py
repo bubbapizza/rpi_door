4f48f23362b7fd91d0218e216c9d8bf75d8e0734
Shawn Wilson <shawn@ch2a.ca>
2014-11-02 22:27:38 -0500

    switch module looks ok, just needs to be tested

--- libaccess/drivers/switch.py
+++ libaccess/drivers/switch.py
@@ -8,15 +8,36 @@ import RPi.GPIO as GPIO
 ON = 1
 OFF = 0
 
-class pushButton():
+class basic():
     """Used for controlling push-button-type switches.  These ones, you
-    can only check the status of the button.  The Raspberry Pi can't set 
-    the switch on or off."""
+    can only check the status of the switch.  The Raspberry Pi can't set 
+    the switch on or off, only the user can."""
+
+    def __init__(self, pin=None, state=OFF):
+        """Initialize an on/off switch.  By default, the starting state
+        is off."""
+
+        self._pin = pin
+        self._state = state
+ 
+        if pin is not None:
+            # initialize the pin and set the state accordingly.
+            GPIO.setup(self._pin, GPIO.IN)
+
+    @property
+    def state(self):
+        """Returns the switch state.  If an RPi pin was specified
+        on intialization, it reads the actual pin state."""
+
+        if self._pin is not None:
+            self._state = GPIO.input(self._pin)
+        return ON if self._state else OFF
 
 
-class onOffSwitch():
-    """Used for manipulating any type of on/off switch.  E.g. a light
-    switch."""
+
+class OnOff(basic):
+    """Used for switches that the RPi can control.  The user may set 
+    them in real life, but we can change them also via code."""
 
     def __init__(self, pin=None, state=OFF):
         """Initialize an on/off switch.  By default, the starting state
@@ -29,22 +50,13 @@ class onOffSwitch():
             # initialize the pin and set the state accordingly.
             GPIO.setup(self._pin, GPIO.OUT)
 
-    @property
-    def state(self):
-        """Returns the switch state.  If an RPi pin was specified
-        on intialization, it reads the actual pin state."""
 
-        if self._pin is not None:
-            self._state = GPIO.input(self._pin)
-        return ON if self._state else OFF
-
-
-    def toggle(self, state=None):
-        """Toggle the switch on or off.  If an ON or OFF state is 
+    def flick(self, state=None):
+        """Flick the switch on or off.  If an ON or OFF state is 
         specified explicitly, then set it accordingly."""
 
         # If no state is explicitly specified, set the pin to whatever
-        # the opposite state is.  i.e., duh, toggle it.
+        # the opposite state is.  i.e., flick the switch  
         if state is None:
             if self.state == ON:
                 GPIO.output(self._pin, GPIO.LOW)    
