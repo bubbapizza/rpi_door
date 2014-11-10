e6bcdaf1257485d20e8a9b2dbc48afb3ccf3ef48
Shawn Wilson <shawn@rj11.ca>
2014-11-09 20:38:52 -0500

    Added comments and doc strings

--- libaccess/doorController.py
+++ libaccess/doorController.py
@@ -1,7 +1,23 @@
-# Copyright (C) 2013 Windsor Hackforge
 #
-# This module is part of RPi Door and is released under
-# the MIT License: http://www.opensource.org/licenses/mit-license.php
+#        Copyright (C) 2014 Shawn Wilson
+#        shawn@rj11.ca
+#        
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This module will allow you to run a door controller using devices 
+and databases provided by libaccess.  libaccess provides drivers for
+RRGBDL (rfid/red/green/buzzer/door/lock-button) devices and various 
+SQL databases."""
 
 from time import sleep
 
--- libaccess/drivers/switch.py
+++ libaccess/drivers/switch.py
@@ -1,6 +1,6 @@
 #
 #        Copyright (C) 2014 Shawn Wilson
-#        shawn@ch2a.ca
+#        shawn@rj11.ca
 #        
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
